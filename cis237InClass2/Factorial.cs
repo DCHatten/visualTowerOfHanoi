2accc4f994223bb6f9fddcb55b7d239834d0d765
Brandon Rodriguez <brodriguez8774@mail.kvcc.edu>
2015-09-22 15:01:08 -0400

    Adds Factorial class and Calculate method.

---
+++ cis237InClass2/Factorial.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237InClass2
+{
+    /// <summary>
+    /// Shows use of recursion to solve factorials.
+    /// </summary>
+    class Factorial
+    {
+        /// <summary>
+        /// Calculates the factorial of the given number.
+        /// </summary>
+        /// <param name="number">The factorial to calculate.</param>
+        /// <returns>The calculated factorial.</returns>
+        public int Calculate(int number)
+        {
+            if (number == 1)
+            {
+                return number;
+            }
+
+            return number * Calculate(number - 1);
+        }
+    }
+}
--- cis237InClass2/Program.cs
+++ cis237InClass2/Program.cs
@@ -8,8 +8,36 @@ namespace cis237InClass2
 {
     class Program
     {
+        /// <summary>
+        /// Main.
+        /// </summary>
+        /// <param name="args">Main-line args.</param>
         static void Main(string[] args)
         {
+            Factorial factorial = new Factorial();
+
+            Console.WriteLine("Input a number to calculate the factorial of: " + "     " + "Note: Must be 30 or lower.");
+
+            try
+            {
+                int factorialInt = Convert.ToInt32(Console.ReadLine().Trim());
+
+                if (factorialInt < 31)
+                {
+                    Console.WriteLine(Environment.NewLine + "Problem: " + factorialInt + "!");
+                    int answerInt = factorial.Calculate(factorialInt);
+
+                    Console.WriteLine(Environment.NewLine + "The answer is: " + answerInt + Environment.NewLine);
+                }
+                else
+                {
+                    Console.WriteLine(Environment.NewLine + "You must enter a number 30 or lower." + Environment.NewLine);
+                }
+            }
+            catch
+            {
+                Console.WriteLine(Environment.NewLine + "You must enter a number 30 or lower." + Environment.NewLine);
+            }
         }
     }
 }
